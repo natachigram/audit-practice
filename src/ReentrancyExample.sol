8fb68b68157712e5b910b23d391402dbb0902869
natachigram <natachigram@gmail.com>
2023-02-20 14:35:06 +0100

    Modified contracts and added POCs

--- src/ReentrancyExample.sol
+++ src/ReentrancyExample.sol
@@ -1,8 +1,9 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity 0.8.0;
 
 contract ReentrancyExample {
   mapping(address => uint) balances;
+  mapping(address => bool) locked;
 
   function deposit() public payable {
     balances[msg.sender] += msg.value;
@@ -10,9 +11,12 @@ contract ReentrancyExample {
 
   function withdraw(uint amount) public {
     require(balances[msg.sender] >= amount, "Insufficient balance.");
+    require(!locked[msg.sender], "Reentrancy detected.");
+    locked[msg.sender] = true;
+    balances[msg.sender] -= amount;
     (bool success, ) = msg.sender.call{value: amount}("");
     require(success, "Transfer failed.");
-    balances[msg.sender] -= amount;
+    locked[msg.sender] = false;
   }
 
   function getBalance() public view returns (uint) {
